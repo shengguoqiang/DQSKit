9d59cab838cab1b868e01b7841cb6c70e9b7048d
dqs <18705147127@163.com>
2019-01-07 14:23:45 +0800

    支持Carthage

---
+++ Example/DQSKit_Carthage/DQSKit_Carthage.h
@@ -0,0 +1,19 @@
+//
+//  DQSKit_Carthage.h
+//  DQSKit_Carthage
+//
+//  Created by 大强神 on 2019/1/7.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DQSKit_Carthage.
+FOUNDATION_EXPORT double DQSKit_CarthageVersionNumber;
+
+//! Project version string for DQSKit_Carthage.
+FOUNDATION_EXPORT const unsigned char DQSKit_CarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DQSKit_Carthage/PublicHeader.h>
+
+
